9d9526aac4020cb514711432c9ca1068c2e294cc
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2025-01-20 16:22:57 +0530

    css changes for copy code alert

--- src/components/SyntaxHighlighterComponent.jsx
+++ src/components/SyntaxHighlighterComponent.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import './SyntaxHighlighterComponent.css'
 
 const SyntaxHighlighterComponent = ({ Response }) => {
+  const [showAlert, setshowAlert] = useState(false)
   const handleCopy = () => {
-    if (Response) {
-      navigator.clipboard
-        .writeText(Response)
-        .then(() => {
-          alert("Code copied to clipboard!");
-        })
-        .catch((err) => {
-          console.error("Failed to copy text: ", err);
-        });
-    } else {
-      alert("No code to copy!");
+
+    if(!Response){
+      setshowAlert(true)
+      setTimeout(() => {
+        setshowAlert(false)
+      }, 3000);
+      return;
     }
+
+    navigator.clipboard.writeText(Response || "// no code to display")
+    setshowAlert(true)
+      setTimeout(() => {
+        setshowAlert(false)
+      }, 3000);
   };
     return (
         <div style={{ position: "relative" }}>
@@ -25,6 +28,13 @@ const SyntaxHighlighterComponent = ({ Response }) => {
       >
         Copy Code
       </button>
+      {
+        showAlert && (
+          <div className="alert-box">
+          { Response ? "Code Copied" : " No Code to Copy "}
+          </div>
+        )
+      }
           <SyntaxHighlighter language="css" style={vscDarkPlus}>
             {Response || "// No code to display."}
           </SyntaxHighlighter>
